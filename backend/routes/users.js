315c9772ffee06134f6253bbca40e88f4f2500e5
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-12-11 19:55:18 +0530

    backend completed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-let Exercise = require('../models/exercise.model.js');
+let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
-    Exercise.find()
-        .then(exercises => res.json(exercises))
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const newUser = new User({ username });
+
+    newUser.save()
+        .then(() => res.json('user added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 })
 
